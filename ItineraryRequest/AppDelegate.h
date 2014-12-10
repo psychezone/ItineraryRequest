f5fc1d2260fe124c0140130720736145ed4799ab
psychezone <spcnet@gmail.com>
2014-12-10 12:42:39 -0500

    Initial Commit

---
+++ ItineraryRequest/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  ItineraryRequest
+//
+//  Created by Shao Chen on 12/10/14.
+//  Copyright (c) 2014 Shao Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ ItineraryRequest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ItineraryRequest
+//
+//  Created by Shao Chen on 12/10/14.
+//  Copyright (c) 2014 Shao Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
